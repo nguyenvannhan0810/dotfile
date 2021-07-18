fcb361d6cdbb887212ec9fd08120900a9ace4a9d
Nguyễn Văn Nhàn <nguyenvannhan@Nguyens-MacBook-Pro.local>
2021-07-18 12:45:35 +0700

    Update neovim config

---
+++ configs/nvim/after/plugin/treesitter.rc.vim
@@ -0,0 +1,25 @@
+if !exists('g:loaded_nvim_treesitter')
+  finish
+endif
+
+lua << EOF
+require 'nvim-treesitter.configs'.setup {
+  hightlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "json",
+    "yaml",
+    "swift",
+    "html",
+    "scss",
+  }
+}
+EOF
--- plugins/neovim.sh
+++ plugins/neovim.sh
@@ -1,12 +1,16 @@
-brew install neovim
+brew install neovim tree-sitter luajit
+
+brew install tmux
 
 ln -sf /usr/local/bin/nvim /usr/local/bin/vim
 
+echo "---- Install Vim Plug -----"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 echo "--- Install The Siver Searcher ---"
 brew install the_silver_searcher
-
-echo "--- Install PHP-CS-Fixer ----"
-brew install php-cs-fixer
 
 if [ ! -d "${HOME}/.config" ]
 then
