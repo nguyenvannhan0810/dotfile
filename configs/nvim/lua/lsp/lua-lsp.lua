ee40d1db66358165dcf0c67650284320bba3c633
Van Nhan <nguyenvannhan0810@gmail.com>
2022-04-16 12:04:41 +0700

    edit many things

--- configs/nvim/lua/lsp/lua-lsp.lua
+++ configs/nvim/lua/lsp/lua-lsp.lua
@@ -8,7 +8,7 @@ local sumneko_binary = ""
 
 if vim.fn.has("mac") == 1 then
     sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/language-servers/lua-language-server"
-    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/language-servers/lua-language-server/bin/macOS/lua-language-server"
+    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/language-servers/lua-language-server/bin/lua-language-server"
 elseif vim.fn.has("unix") == 1 then
     sumneko_root_path = "/home/" .. USER .. "/.config/nvim/language-servers/lua-language-server"
     sumneko_binary = "/home/" .. USER .. "/.config/nvim/language-servers/lua-language-server/bin/Linux/lua-language-server"
